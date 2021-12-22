4d6d16b2f6ed08ecd4884ebaa1d5ee9a6a376e9a
Matt Hughes <zeepkdb@gmail.com>
2021-12-22 15:37:57 -0800

    added deprecation notice

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'primereact/card';
 import { InputSwitch } from 'primereact/inputswitch';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Snackbar from '@material-ui/core/Snackbar';
 import BugIcon from '../../images/bugIcon.png';
 import SeaIcon from '../../images/sea.png';
 import FishIcon from '../../images/fishIcon.png';
@@ -15,11 +18,41 @@ import '../../styles/Header.css';
 class Header extends Component {
 	state = {
 		hideCaught: false,
+		open: true
 	};
 
 	render() {
+		const message = <div className='text'>{"Attention: if you use this site please start using the new version at\n"}
+			<a href="https://acwildlife-dev.netlify.app" className='link'>
+				{' acwildlife-dev.netlify.app '}
+			</a>
+			{"\n(it will be replacing this one soon)\n\nYou can create an account and even import your data from this version in your account settings!\n\nFeel free to"}
+			<a href="https://twitter.com/matthughes2112" className='link'>
+				{' DM me on twitter '}
+			</a>
+			{"if you have an trouble/questions. Thanks! :)"}</div>
 		return (
 			<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+				<Snackbar
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'center',
+					}}
+					open={this.state.open}
+					message={message}
+					action={
+						<React.Fragment>
+							<IconButton
+								size="small"
+								aria-label="close"
+								color="inherit"
+								onClick={() => this.setState({ open: false })}
+							>
+								<CloseIcon fontSize="small" />
+							</IconButton>
+						</React.Fragment>
+					}
+				/>
 				<Link className="navbar-brand" to="/">
 					<p className="title">WildLife Tracker</p>
 				</Link>
